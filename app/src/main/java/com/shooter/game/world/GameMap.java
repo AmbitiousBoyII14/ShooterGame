d9670d43e9c2392d355905be4bc6be91eae90c7b
AmbitiousBoyII14 <132277700+AmbitiousBoyII14@users.noreply.github.com>
2026-09-03 01:09:10 +0200

    Automated upload: GameMap.java

---
+++ app/src/main/java/com/shooter/game/world/GameMap.java
@@ -0,0 +1,32 @@
+package com.shooter.game.world;
+
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.utils.Array;
+
+public class GameMap {
+    public Array<Platform> platforms;
+    public float worldWidth;
+    public float worldHeight;
+
+    public GameMap() {
+        platforms = new Array<Platform>();
+        worldWidth = 1280;
+        worldHeight = 720;
+
+        platforms.add(new Platform(0, 0, worldWidth, 50)); // Ground
+        platforms.add(new Platform(0, 50, 50, worldHeight - 50)); // Left Wall
+        platforms.add(new Platform(worldWidth - 50, 50, 50, worldHeight - 50)); // Right Wall
+
+        platforms.add(new Platform(200, 200, 300, 30));
+        platforms.add(new Platform(600, 350, 300, 30));
+        platforms.add(new Platform(950, 200, 250, 30));
+    }
+
+    public void render(ShapeRenderer shapeRenderer) {
+        shapeRenderer.setColor(0.3f, 0.3f, 0.3f, 1);
+        for (int i = 0; i < platforms.size; i++) {
+            Platform p = platforms.get(i);
+            shapeRenderer.rect(p.bounds.x, p.bounds.y, p.bounds.width, p.bounds.height);
+        }
+    }
+}
