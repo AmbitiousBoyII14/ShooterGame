b08c846255aeb536fe64ed479ab4bacb30af1187
AmbitiousBoyII14 <132277700+AmbitiousBoyII14@users.noreply.github.com>
2026-09-03 02:31:39 +0200

    Add missing MainGame.java

--- app/src/main/java/com/shooter/game/MainGame.java
+++ app/src/main/java/com/shooter/game/MainGame.java
@@ -1,6 +1,11 @@
 package com.shooter.game;
+
 import com.badlogic.gdx.Game;
 import com.shooter.game.screens.GameScreen;
+
 public class MainGame extends Game {
-    @Override public void create() { setScreen(new GameScreen(this)); }
+    @Override
+    public void create() {
+        setScreen(new GameScreen(this));
+    }
 }
