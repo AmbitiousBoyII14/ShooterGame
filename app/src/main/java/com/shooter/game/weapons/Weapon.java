abf92f36bfc58e932d94126735fdad40d236e096
AmbitiousBoyII14 <132277700+AmbitiousBoyII14@users.noreply.github.com>
2026-09-03 02:18:02 +0200

    Final fix: Weapon.java

--- app/src/main/java/com/shooter/game/weapons/Weapon.java
+++ app/src/main/java/com/shooter/game/weapons/Weapon.java
@@ -1,45 +1,13 @@
 package com.shooter.game.weapons;
-
 import com.badlogic.gdx.utils.Array;
 import com.shooter.game.bullets.Bullet;
-
 public abstract class Weapon {
-    public String name;
-    public float damage;
-    public float fireRate; 
-    public float fireCooldown;
-    public int magSize;
-    public int currentAmmo;
-    public int reserveAmmo;
-    public float reloadTime;
-    public float reloadTimer;
-    public boolean isReloading;
-    public float bulletSpeed;
-    public float spread;
-
-    public Weapon() {
-        fireCooldown = 0;
-        isReloading = false;
-        reloadTimer = 0;
-    }
-
+    public String name; public float damage, fireRate, fireCooldown, reloadTime, reloadTimer, bulletSpeed, spread;
+    public int magSize, currentAmmo, reserveAmmo; public boolean isReloading;
+    public Weapon() { fireCooldown = 0; isReloading = false; reloadTimer = 0; }
     public abstract void fire(float x, float y, float dirX, float dirY, Array<Bullet> bulletPool);
-
     public void update(float delta) {
         if (fireCooldown > 0) fireCooldown -= delta;
-        if (isReloading) {
-            reloadTimer -= delta;
-            if (reloadTimer <= 0) {
-                int needed = magSize - currentAmmo;
-                if (reserveAmmo >= needed) {
-                    currentAmmo = magSize;
-                    reserveAmmo -= needed;
-                } else {
-                    currentAmmo += reserveAmmo;
-                    reserveAmmo = 0;
-                }
-                isReloading = false;
-            }
-        }
+        if (isReloading) { reloadTimer -= delta; if (reloadTimer <= 0) { int needed = magSize - currentAmmo; if (reserveAmmo >= needed) { currentAmmo = magSize; reserveAmmo -= needed; } else { currentAmmo += reserveAmmo; reserveAmmo = 0; } isReloading = false; } }
     }
 }
