ba66f825ba027f4c2663ae929c879893b95a29c9
AmbitiousBoyII14 <132277700+AmbitiousBoyII14@users.noreply.github.com>
2026-09-03 02:06:31 +0200

    Fix imports and dependencies

--- app/src/main/java/com/shooter/game/MainActivity.java
+++ app/src/main/java/com/shooter/game/MainActivity.java
@@ -1,12 +1,9 @@
 package com.shooter.game;
-
 import android.os.Bundle;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
-
 public class MainActivity extends AndroidApplication {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    @Override protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
         initialize(new MainGame(), config);
